14f2f3b998e6c6c2b1adf9c496f630fe4e22fa8c
subeerkumar007 <subeerk491@gmail.com>
2026-05-23 10:58:14 +0530

    both side

---
+++ backend/src/db/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+    }
+    catch (error) {
+        console.log('Database connection error:', error);
+    }
+}
+module.exports = connectDB;
